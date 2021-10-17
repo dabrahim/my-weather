2bb2efb70fa6fdeaa8142873883735c635197758
Ibrahima NDIAYE <ibrahima.ndiaye450@gmail.com>
2021-10-17 16:59:58 +0000

    Simplified ViewModel events management

--- app/src/main/java/tech/keyops/challenge/myweather/ui/utility/Event.kt
+++ app/src/main/java/tech/keyops/challenge/myweather/ui/utility/Event.kt
@@ -1,5 +1,7 @@
 package tech.keyops.challenge.myweather.ui.utility
 
+import androidx.lifecycle.Observer
+
 /**
  * This class is used as a data wrapper and a means of communication
  * between the ViewModel and the View.
@@ -24,4 +26,18 @@ open class Event<out T>(private val content: T) {
      * Returns the content, even if it's already been handled.
      */
     fun peekContent(): T = content
+}
+
+/**
+ * An [Observer] for [Event]s, simplifying the pattern of checking if the [Event]'s content has
+ * already been handled.
+ *
+ * [onEventUnhandledContent] is *only* called if the [Event]'s contents has not been handled.
+ */
+class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.getContentIfNotDispatchedOrReturnNull()?.let {
+            onEventUnhandledContent(it)
+        }
+    }
 }
